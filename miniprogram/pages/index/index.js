f7d14e0b817e48bae43d95e52e1638121c65806b
guanjianli <guanjianli@gmail.com>
2019-01-22 00:09:59 +0800

    first add

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,30 @@
+const app = getApp();
+Page({
+  data: {
+    statusBarHeight: app.sysInfo.statusBarHeight
+  },
+  onLoad: function (options) {
+
+  },
+  onReady: function () {
+
+  },
+  onShow: function () {
+
+  },
+  onHide: function () {
+
+  },
+  onUnload: function () {
+
+  },
+  onPullDownRefresh: function () {
+
+  },
+  onReachBottom: function () {
+
+  },
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ miniprogram/pages/me/me.js
@@ -0,0 +1,33 @@
+
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  onGetUserInfo: function (e) {
+    if (e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+})
